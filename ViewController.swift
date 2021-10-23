c0ebb8b28220005972806c97d8db05bbaaa60bbd
Bruno Vieira <brunoscvieira@gmail.com>
2021-10-23 19:18:32 -0300

    creating pod

---
+++ ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  miniBanking_core
+//
+//  Created by Bruno Vieira on 23/10/21.
+//
+
+import UIKit
+
+open class ViewController: UIViewController {
+        
+    public init() {
+        super.init(
+            nibName: nil,
+            bundle: nil
+        )
+    }
+    
+    public required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+}
